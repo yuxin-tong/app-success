5aac29e4f6e22e822f60d7056289b60e041ab56d
Mahmud <mahmud.masum@chamonix.com.au>
2022-03-01 15:48:17 +1030

    Dashboard module and routing

--- src/app/modules/user-area/user-area-routing.module.ts
+++ src/app/modules/user-area/user-area-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardPageComponent } from './modules/dashboard/pages/dashboard-page/dashboard-page.component';
 import { UserAreaPageComponent } from './pages/user-area-page/user-area-page.component';
 
-const routes: Routes = [{ path: '', component: UserAreaPageComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: UserAreaPageComponent,
+    loadChildren: () =>
+      import('./modules/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/user-area/user-area.module.ts
+++ src/app/modules/user-area/user-area.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { UserAreaRoutingModule } from './user-area-routing.module';
 import { UserAreaPageComponent } from './pages/user-area-page/user-area-page.component';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [UserAreaPageComponent],
-  imports: [CommonModule, UserAreaRoutingModule],
+  imports: [CommonModule, UserAreaRoutingModule, DashboardModule],
 })
 export class UserAreaModule {}
