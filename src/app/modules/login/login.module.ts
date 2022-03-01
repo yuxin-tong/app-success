fd47e4c60f8f9bf0300626e6127f3f38288be2aa
Mahmud <mahmud.masum@chamonix.com.au>
2022-03-01 11:29:07 +1030

    Login component and Login page

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    LoginPageComponent
+  ],
   imports: [
     CommonModule,
     LoginRoutingModule
