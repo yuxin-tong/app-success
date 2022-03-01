f2579817666838b1c8f4bcae71992b1be61716d8
Mahmud <mahmud.masum@chamonix.com.au>
2022-03-01 11:52:02 +1030

    Adding user-area routing

--- src/app/modules/user-area/user-area-routing.module.ts
+++ src/app/modules/user-area/user-area-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserAreaPageComponent } from './pages/user-area-page/user-area-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: UserAreaPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class UserAreaRoutingModule { }
+export class UserAreaRoutingModule {}
--- src/app/modules/user-area/user-area.module.ts
+++ src/app/modules/user-area/user-area.module.ts
@@ -2,18 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserAreaRoutingModule } from './user-area-routing.module';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { UserAreaPageComponent } from './pages/user-area-page/user-area-page.component';
 
-
 @NgModule({
-  declarations: [
-    DashboardComponent,
-    UserAreaPageComponent
-  ],
-  imports: [
-    CommonModule,
-    UserAreaRoutingModule
-  ]
+  declarations: [UserAreaPageComponent],
+  imports: [CommonModule, UserAreaRoutingModule],
 })
-export class UserAreaModule { }
+export class UserAreaModule {}
