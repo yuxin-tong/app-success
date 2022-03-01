ef331b930390952b462653996ef67956db0be6fa
Mahmud <mahmud.masum@chamonix.com.au>
2022-03-01 11:31:11 +1030

    Adding login route

--- src/app/modules/login/login-routing.module.ts
+++ src/app/modules/login/login-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'login', component: LoginPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class LoginRoutingModule { }
+export class LoginRoutingModule {}
