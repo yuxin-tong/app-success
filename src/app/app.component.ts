d12a96375bf5eaa177ee5ecb2b88f2fae5b43061
Mahmud <mahmud.masum@chamonix.com.au>
2022-03-22 23:27:54 +1030

    Separate layout for each master page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { RoutingConstants } from './core/constants/routing.constants';
+import { AuthenticationService } from './modules/authentication/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,20 @@ import { RoutingConstants } from './core/constants/routing.constants';
 export class AppComponent {
   title = 'MySATAC';
   authArea = false;
-  constructor(private router: Router) {
+  registrationArea = false;
+  publicArea = false;
+  constructor(
+    private router: Router,
+    public authService: AuthenticationService
+  ) {
     this.router.events.subscribe((e) => {
       if (e instanceof NavigationEnd) {
         this.authArea = e.urlAfterRedirects.includes(RoutingConstants.AUTH);
+        this.registrationArea = e.urlAfterRedirects.includes(
+          RoutingConstants.REGISTRATION
+        );
+
+        this.publicArea = this.authArea || this.registrationArea;
       }
     });
   }
