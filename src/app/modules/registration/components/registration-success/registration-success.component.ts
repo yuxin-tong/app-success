75897252eab54f62cc9899aadb78bc2afdb0fbaf
Mahmud <mahmud.masum@chamonix.com.au>
2022-03-24 12:33:00 +1030

    Resend registration

--- src/app/modules/registration/components/registration-success/registration-success.component.ts
+++ src/app/modules/registration/components/registration-success/registration-success.component.ts
@@ -9,14 +9,20 @@ import { RoutingConstants } from 'src/app/core/constants/routing.constants';
 })
 export class RegistrationSuccessComponent implements OnInit {
   constructor(private router: Router) {}
-
+  resendRegistrationEmailPath = `/${RoutingConstants.REGISTRATION}/${RoutingConstants.RESEND_VERIFICATION_EMAIL}`;
   email = '';
 
   ngOnInit(): void {
     if (!history.state || !history.state.email) {
-      this.router.navigate([RoutingConstants.REGISTRATION]);
+      //  this.router.navigate([RoutingConstants.REGISTRATION]);
     } else {
       this.email = history.state.email;
     }
+  }
+
+  navigateToResend() {
+    this.router.navigate([this.resendRegistrationEmailPath], {
+      state: { email: 'test' },
+    });
   }
 }
--- src/app/modules/registration/components/resend-verification-email/resend-verification-email.component.ts
+++ src/app/modules/registration/components/resend-verification-email/resend-verification-email.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RoutingConstants } from 'src/app/core/constants/routing.constants';
+import { SpinnerService } from 'src/app/core/services/spinner.service';
+import { RegistrationService } from '../../registration.service';
 
 @Component({
   selector: 'app-resend-verification-email',
   templateUrl: './resend-verification-email.component.html',
-  styleUrls: ['./resend-verification-email.component.scss']
+  styleUrls: ['./resend-verification-email.component.scss'],
 })
 export class ResendVerificationEmailComponent implements OnInit {
+  email = '';
+  submitted = false;
+  serverError = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private registrationService: RegistrationService,
+    public spinnerService: SpinnerService
+  ) {
+    if (!history.state || !history.state.email) {
+      this.router.navigate([RoutingConstants.LOGIN]);
+    } else {
+      this.email = history.state.email;
+    }
   }
 
+  ngOnInit(): void {}
+
+  submit() {
+    this.spinnerService.show();
+    this.registrationService
+      .resendVerificationEmail(this.email)
+      .subscribe((resp) => {
+        if (resp.statusCode == 200) {
+          this.submitted = true;
+        } else {
+          this.serverError = true;
+        }
+      });
+  }
 }
